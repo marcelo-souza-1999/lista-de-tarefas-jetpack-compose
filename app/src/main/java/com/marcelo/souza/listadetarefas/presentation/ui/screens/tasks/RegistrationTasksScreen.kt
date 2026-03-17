6282b1755c555ff3d31471fb6a10f85fcb8d1857
Marcelo Souza <developermarcelosouza@gmail.com>
2026-03-16 21:53:20 -0300

    fix: avoid persisting success dialog state across config change

--- app/src/main/java/com/marcelo/souza/listadetarefas/presentation/ui/screens/tasks/RegistrationTasksScreen.kt
+++ app/src/main/java/com/marcelo/souza/listadetarefas/presentation/ui/screens/tasks/RegistrationTasksScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
@@ -64,7 +63,7 @@ fun RegistrationTaskScreen(
         }
     }
 
-    var showSuccessDialog by rememberSaveable { mutableStateOf(false) }
+    var showSuccessDialog by remember { mutableStateOf(false) }
     var errorToDisplay by remember { mutableStateOf<DataError?>(null) }
 
     LaunchedEffect(Unit) {
